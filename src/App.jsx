00097c0846b246367ebd4c0d17381db65b765f9d
UpgradeOttawa <107906673+UpgradeOttawa@users.noreply.github.com>
2026-08-24 08:37:54 -0400

    Make material picker the HelpOttawa homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,35 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import LandingPage from './pages/LandingPage';
+import MaterialPicker from './MaterialPicker';
 import BathroomCalculator from './BathroomCalculator';
 import BathroomVisualDesigner from './BathroomVisualDesigner';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import ContractorsPage from './pages/ContractorsPage';
 
+function LegacyPage({ children }) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/bathroom-calculator" element={<BathroomCalculator />} />
-            <Route path="/bathroom-designer" element={<BathroomVisualDesigner />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/contractors" element={<ContractorsPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<MaterialPicker />} />
+        <Route path="/materials" element={<MaterialPicker />} />
+        <Route path="/bathroom-calculator" element={<LegacyPage><BathroomCalculator /></LegacyPage>} />
+        <Route path="/bathroom-designer" element={<LegacyPage><BathroomVisualDesigner /></LegacyPage>} />
+        <Route path="/about" element={<LegacyPage><AboutPage /></LegacyPage>} />
+        <Route path="/contact" element={<LegacyPage><ContactPage /></LegacyPage>} />
+        <Route path="/contractors" element={<LegacyPage><ContractorsPage /></LegacyPage>} />
+      </Routes>
     </Router>
   );
 }
